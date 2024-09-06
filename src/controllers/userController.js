623ea8c239fa3c8e58f9eb05e64fb055a8147def
Byron Gonzalez <byrondein@gmail.com>
2024-09-05 20:32:56 -0400

    add user models, controller and routers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -46,7 +46,6 @@ export const  verifyToken = (req, res, next) => {
     if (!token) {
         return res.status(401).json({ message: 'Access denied' });
     }
-
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
