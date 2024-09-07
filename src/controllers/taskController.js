43448cb041d6011cbc613c8a1c972801cba15f8b
Byron Gonzalez <byrondein@gmail.com>
2024-09-07 15:41:41 -0400

    fix models

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,5 +1,4 @@
-import Task from '../models/task.js';
-
+import Task from '../models/Task.js';
 export const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.findAll();
