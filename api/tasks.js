bc49de7803b01f5aaa08fe6dbd2850623803d627
Byron Gonzalez <byrondein@gmail.com>
2024-09-11 23:07:15 -0300

    add api folder

--- api/tasks.js
+++ api/tasks.js
@@ -1,14 +1,29 @@
 import { getAllTasks, createTask, getTaskById, updateTask, deleteAllTasks, deleteTaskById } from '../src/controllers/taskController.js';
-import express from 'express';
 
-const app = express();
-app.use(express.json());
-
-app.get('/api/tasks', getAllTasks);
-app.post('/api/tasks', createTask);
-app.get('/api/tasks/:id', getTaskById);
-app.patch('/api/tasks/:id', updateTask);
-app.delete('/api/tasks', deleteAllTasks);
-app.delete('/api/tasks/:id', deleteTaskById);
-
-export default app;
+export default async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      if (req.query.id) {
+        await getTaskById(req, res);
+      } else {
+        await getAllTasks(req, res);
+      }
+      break;
+    case 'POST':
+      await createTask(req, res);
+      break;
+    case 'PATCH':
+      await updateTask(req, res);
+      break;
+    case 'DELETE':
+      if (req.query.id) {
+        await deleteTaskById(req, res);
+      } else {
+        await deleteAllTasks(req, res);
+      }
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PATCH', 'DELETE']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
